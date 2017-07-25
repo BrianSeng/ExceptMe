908815d4c2a8071e61bb8cea4a7080b35c012cbe
BrianSeng <bsengsourichanh@gmail.com>
2017-07-25 01:07:33 -0700

    db check before init

--- Logger.Domain/ExceptionDbContext.cs
+++ Logger.Domain/ExceptionDbContext.cs
@@ -2,8 +2,12 @@
 
 namespace Logger.Domain
 {
-    public class ExceptionContext : DbContext
+    public class ExceptionDbContext : DbContext
     {
+        public ExceptionDbContext(): base("DefaultConnection") 
+        {
+            Database.SetInitializer<ExceptionDbContext>(new CreateDatabaseIfNotExists<ExceptionDbContext>());
+        }
         public DbSet<Exception> Exceptions { get; set; }
     }
 }
